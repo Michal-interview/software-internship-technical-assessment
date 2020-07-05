da9628b8ba54962aa63a4143f3c9eb5cbc6ca81f
Michal-interview <1005michal@wp.pl>
2020-07-05 18:51:17 +0200

    Final changes

--- michal.ts
+++ michal.ts
@@ -1,55 +1,44 @@
 import * as readline from 'readline'
 
-/* Does not work for the test, but trying indivdual inputs gives the correct result
-
-rl.question('Please enter the number : ', (answer) => {
-    let validInput = validateInput(answer)
-    if (luhnAlgorithm(validInput)) {
-        exit(true, 'The number given is not valid')
-    } else {
-        exit(false, 'The number given is not valid')
-    }
-    rl.close()
-})
-*/
-
-var rl = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 })
-  
-rl.on('line', function(line){
-      
-      let validInput: number[] = validateInput(line)
 
-      if (luhnAlgorithm(validInput)) {
+rl.on('line', (line: string): void => {
+    const validInput: number[] = validateInput(line)
+
+    if (isLuhnNumber(validInput)) {
         result(true, 'The number is valid')
-      } else {
-        result(false, 'The number is not valid')
-      }
-  })
+    }
+
+    result(false, 'The number is not valid')
+})
 
 function validateInput(input: string): number[] {
+    const noSpaces = input.replace(/\s/g, '')
+    const isNum = /^\d+$/.test(noSpaces)
+    const isShort = noSpaces.length <= 1
 
-    let noSpaces = input.replace(/\s/g, '')
-    
-    let isNum = /^\d+$/.test(noSpaces)
-    if (!isNum) result(false, "Input contains a character that is not a digit")
+    if (!isNum) {
+        result(false, "Input contains a character that is not a digit")
+    }
 
-    let isShort = noSpaces.length <= 1
-    if (isShort) result(false, "Strings of length 1 or less are not valid")
+    if (isShort) {
+        result(false, "Strings of length 1 or less are not valid")
+    }
 
     return noSpaces.split('').map(Number)
 }
 
-function luhnAlgorithm(nums: number[]): boolean {
-    let luhnNums = nums
-    for (let i = nums.length - 2; i > -1; i = i - 2) {
-        let doubledDigit = 2 * luhnNums[i]
+function isLuhnNumber(nums: number[]): boolean {
+    let luhnNums = [...nums]
+    for (let i = luhnNums.length - 2; i > -1; i = i - 2) {
+        const doubledDigit = 2 * luhnNums[i]
         luhnNums[i] = (doubledDigit > 9) ? ( doubledDigit - 9) : (doubledDigit)
     }
     let numsSum = luhnNums.reduce((a, b) => a + b, 0)
-    return numsSum % 10 == 0 
+    return numsSum % 10 == 0
 }
 
 function result(isValid: boolean, comment: string): void {
